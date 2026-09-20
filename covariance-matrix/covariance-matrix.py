10a26c9fb43b493a45f98a45a2b3cb325e84475d
Althariq Fairuz <112919962+AlthariqFairuz@users.noreply.github.com>
2026-09-20 09:57:40 +0700

    Solve covariance-matrix

--- covariance-matrix/covariance-matrix.py
+++ covariance-matrix/covariance-matrix.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 def covariance_matrix(X: list) -> np.ndarray:
-    """Return the sample covariance matrix of X."""
+    """
+    Returns the covariance matrix as a NumPy array.
+    """
+    # Write code here
     X = np.asarray(X, dtype=float)
-    center = X- np.mean(X, axis = 0)
+    center = X-np.mean(X, axis = 0)
     sigma = center.T @ center / (center.shape[0] - 1)
     return sigma
+    
